5d4945c1307246209af9a16c310177e2c8992d7a
Eduardo González <alu0101319001@ull.edu.es>
2022-03-28 09:50:29 +0000

    Completo Mod p7

--- tests/mod1.spec.ts
+++ tests/mod1.spec.ts
@@ -9,28 +9,27 @@ context('Modificación P7', () => {
 			expect(primeNumberInstace.firstPrimes(5)).to.be.eql([2, 3, 5, 7, 11]);
 		});
 
-		it('Bad input return the prev value', () => {
+		it('Bad input return []', () => {
 			const primeNumberInstace = PrimeNumber.getPrimeNumber();
-			const test = [5, 7, 11];
-			expect(primeNumberInstace.firstPrimes(-2)).to.be.eql(test);
+			expect(primeNumberInstace.firstPrimes(-2)).to.be.eql([]);
 		});
-
+		
 		it('Primos entre 4 y 30 = [5, 7, 11, 13, 17, 19, 23, 29]', () => {
 			const primeNumberInstace = PrimeNumber.getPrimeNumber();
 			const test = [5, 7, 11, 13, 17, 19, 23, 29];
 			expect(primeNumberInstace.rangePrimes(4, 30)).to.be.eql(test);
 		});
-
-		it('Bad input return the prev value', () => {
+		
+		it('Bad input return []', () => {
 			const primeNumberInstace = PrimeNumber.getPrimeNumber();
-			const test = [5, 7, 11, 13, 17, 19, 23, 29];
-			expect(primeNumberInstace.rangePrimes(-2, -1)).to.be.eql(test);
+			expect(primeNumberInstace.rangePrimes(-2, -1)).to.be.eql([]);
 		});
 	});
 
 	describe('PrimeNumber Iterable Class', () => {
 		it('Iterar sobre PrimeNumber', () => {
 			const primeNumberInstace = PrimeNumber.getPrimeNumber();
+			primeNumberInstace.rangePrimes(4, 30);
 			const test: number[] = [];
 			[...primeNumberInstace].forEach((number) => test.push(number));
 			expect(test).to.be.eql([5, 7, 11, 13, 17, 19, 23, 29]);
