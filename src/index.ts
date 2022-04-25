936a1cc18f23c3e18007926b212a79705d5d635c
Eduardo González <alu0101319001@ull.edu.es>
2022-04-25 10:02:34 +0000

    Modificacion P10

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,33 @@
-import {Reduce} from './reduce';
-import {AddReduce} from './addreduce';
-import {SubReduce} from './subreduce';
-import {ProdReduce} from './prodreduce';
-import {DivReduce} from './divreduce';
+import {accessSync, constants, watchFile} from 'fs';
+import {spawn} from 'child_process';
 
-function clientCode(reduceFunction: Reduce) {
-    reduceFunction.run;
+if (process.argv.length !== 4) {
+  console.log('Please, specify a file and select a field');
+} else {
+  const filename = process.argv[2];
+	const field = process.argv[3];
+
+	try {
+		accessSync(filename, constants.F_OK); 
+		console.log(`Starting to watch file ${filename}`);
+
+		watchFile(`${filename}`, () => {
+			console.log(`\nFile ${filename} has been modified`);
+			const cut = spawn('cut', ['-d', ',', '-f', `${field}`, `${filename}`]);
+			let cutOutput = '';
+			cut.stdout.on('data', (piece) => cutOutput += piece);
+			// cut.stdout.pipe(process.stdout);
+
+			cut.on('close', () => {
+				const cutArray = cutOutput.split(/\s+/);
+				if (cutArray.length === 0) {
+					console.log(`The field is not valid`);
+				} else {
+					console.log(cutArray.toString());
+				}
+			});
+		});
+	} catch (err) {
+		console.log(`File ${filename} does not exist`);
+	}
 }
-
-let array: number[] = [1,2,3,4,5];
-
-clientCode(new AddReduce(array));
-clientCode(new SubReduce(array));
-clientCode(new ProdReduce(array));
-clientCode(new DivReduce(array));
